91f37c632d6993b885af3b06679c6c338056e2b7
James Paolantonio <james@howaboutwe.com>
2013-03-19 13:30:00 -0400

    Updated template

---
+++ templates/Kiwi Spec.xctemplate/___FILEBASENAME___Spec.m
@@ -0,0 +1,29 @@
+//
+//  ___FILEBASENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "kiwi.h"
+#import "___FILEBASENAME___.h"
+
+SPEC_BEGIN(___FILEBASENAME___Spec)
+
+describe(@"___FILEBASENAME___", ^{
+    beforeAll(^{
+        //[MagicalRecord setDefaultModelFromClass:[self class]];
+        //[MagicalRecord setupCoreDataStackWithInMemoryStore];
+    });
+    
+    afterAll(^{
+        //[MagicalRecord cleanUp];
+    });
+    
+    context(@"", ^{
+        
+    });
+});
+
+SPEC_END
